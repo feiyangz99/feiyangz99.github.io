5e61386c5fcffbfc8fb2beaeae6e05d12d979e88
feiyangz99 <feiyangz@uchicago.edu>
2023-04-10 04:27:34 -0500

    s

--- js/sampleJS.js
+++ js/sampleJS.js
@@ -2,7 +2,7 @@ function howdy(firstName){
     alert("Howdy"+firstName);
 }
 function conditional(){
-    //alert("Use Inspect to see the console and inspect the code.");
+    alert("Welcome to Feiyang Zheng's Studio.");
     var currentHour = new Date().getHours();
     if (currentHour < 10){
         alert("Good morning!");
