9d3b6b3b9c22f3195fe42ac9aa6c8c69b1277fa6
feiyangz99 <feiyangz@uchicago.edu>
2023-04-10 03:49:53 -0500

    s

--- js/sampleJS.js
+++ js/sampleJS.js
@@ -1,3 +1,14 @@
 function howdy(firstName){
     alert("Howdy"+firstName);
 }
+function conditional(){
+    alert("Use Inspect to see the console and inspect the code.");
+    var currentHour = new Date().getHours();
+    if (currentHour < 10){
+        alert("Good morning!");
+    }   else if (currentHour < 18){
+        alert("Good day!");
+    }   else{
+        alert("Good evening!");
+    }
+}
