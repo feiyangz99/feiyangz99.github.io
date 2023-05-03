bb3b5120de015a3dcaea9b914f1d7a056372db47
feiyangz99 <feiyangz@uchicago.edu>
2023-05-02 20:33:33 -0500

    s

--- js/howl_xc.js
+++ js/howl_xc.js
@@ -3,7 +3,34 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   // Use .getElementsByClassName() to get an HTMLCollection of all .animalTitle elements.
+  const animalTitles = document.getElementsByClassName('animalTitle');
 
+  // Create a new Object with properties and values like 'animal01: "Cow" '.
+  // These values can be found in the HTMLCollection.
+  const animalMap = {};
+  for (let i = 0; i < animalTitles.length; i++) {
+    animalMap[`animal0${i + 1}`] = animalTitles[i].innerText;
+  }
+
+  // Use the Object to create event listeners for <img> elements that launch the provided howler script.
+  for (const animalId in animalMap) {
+    const animalImg = document.getElementById(animalId);
+    const animalSound = animalMap[animalId];
+
+    // Write a single 'for...in' statement to add listeners to all the <img> elements.
+    animalImg.addEventListener('click', () => {
+      playAnimal(animalSound);
+    });
+
+    // Leverage the Object to add mouseover styling to the <img> elements.
+    animalImg.addEventListener('mouseover', () => {
+      animalImg.style.border = '3px solid red';
+    });
+
+    animalImg.addEventListener('mouseout', () => {
+      animalImg.style.border = '0px';
+    });
+  }
   // Create a new Object with properties and values like 'animal01: "Cow" '.
     // These values can be found in the HTMLCollection.
 
@@ -31,4 +58,3 @@ function playAnimal(animalSound) {
   });
   sound.play(animalSound);
 }
-
